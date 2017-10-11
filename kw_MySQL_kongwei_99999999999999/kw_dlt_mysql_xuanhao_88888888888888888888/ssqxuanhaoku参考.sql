186619b4e87b04f85a143bb8d23f2706fcf5f11f
miaoer0028 <32697065+miaoer0028@users.noreply.github.com>
2017-10-11 10:28:01 -0500

    mysql_readme.md

---
+++ kw_MySQL_kongwei_99999999999999/kw_dlt_mysql_xuanhao_88888888888888888888/ssqxuanhaoku参考.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.3
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 25, 2016 at 10:21 AM
+-- Server version: 5.1.62
+-- PHP Version: 5.4.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ssqxuanhaoku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ssqxh1`
+--
+
+CREATE TABLE IF NOT EXISTS `ssqxh1` (
+  `xh1` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ssqxh1`
+--
+
+INSERT INTO `ssqxh1` (`xh1`) VALUES
+(1),
+(2),
+(3),
+(4),
+(5),
+(6),
+(7),
+(8),
+(9),
+(10),
+(11),
+(12),
+(16),
+(15),
+(14),
+(13);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ssqxh6`
+--
+
+CREATE TABLE IF NOT EXISTS `ssqxh6` (
+  `xh6` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ssqxh6`
+--
+
+INSERT INTO `ssqxh6` (`xh6`) VALUES
+(1),
+(2),
+(3),
+(4),
+(5),
+(6),
+(7),
+(8),
+(9),
+(10),
+(11),
+(12),
+(13),
+(14),
+(15),
+(16),
+(17),
+(18),
+(19),
+(20),
+(21),
+(22),
+(23),
+(24),
+(25),
+(26),
+(27),
+(28),
+(29),
+(30),
+(31),
+(32),
+(33);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
